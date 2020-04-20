1a58836822154a4948c7c8c98e2b8d097618cdef
Ondřej Švec <60104439+OndrejSvec@users.noreply.github.com>
2020-04-20 17:47:15 +0200

    V3

--- scripts.js
+++ scripts.js
@@ -4,6 +4,13 @@ console.log(`Hello, my dear ${name}`);
 
 // jQuery part
 
-$('h2').text('The jQuery part has started!');
+$('p').html('The <stribg>jQuery</strong> part has started!');
 
 $('h1').css('background', 'red');
+
+$('h2').toggleClass('green');
+$('p').toggleClass('green');
+$('p').toggleClass('green');
+
+$('main').append('<p>Added elemment</p>');
+$('main').append('<p>Added second elemment</p>');
